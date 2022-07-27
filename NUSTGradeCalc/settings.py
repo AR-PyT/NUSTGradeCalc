9486bd398f567531888689a4daa14392f01a0c09
ARPyT <abrehman.bscs21seecs@seecs.edu.pk>
2022-07-28 04:59:09 +0500

    fully updated

--- NUSTGradeCalc/settings.py
+++ NUSTGradeCalc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=f7n6))9e_yf-ik1ayu+171c!yx^ntk&)r+9o4j+gjomj-wx@1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '']
+ALLOWED_HOSTS = ['127.0.0.1', 'grader-predictor-nust.herokuapp.com']
 
 
 # Application definition
