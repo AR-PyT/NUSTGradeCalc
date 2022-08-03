6f94a8cb762f147d1b188cf9a40bf9960cb03780
ARPyT <abrehman.bscs21seecs@seecs.edu.pk>
2022-08-03 14:54:23 +0500

    fully updated

--- GradeApp/predictions.py
+++ GradeApp/predictions.py
@@ -78,9 +78,14 @@ def make_prediction(score, ch, data):
     grade = np.where(res == np.max(res))[0][0]
 
     # Special Chk for Management Tentative
-    start = 95 if mean > 90 else 89
+    start = 92 if mean > 85 else 89
 
     boundaries = get_boundary(start, ch, mean, data)
+
+    if ch == 2:
+        print('Mean: ', mean)
+        print('Score: ', score)
+        print('Grade: ', grade_map[str(grade)])
     return (grade_map[str(grade)],
             (str(round(100 - (stats.norm.cdf((boundaries[8 - grade] - score) / sqrt(data.var())) * 100),
                        2)) if grade else "0") + "%",
@@ -96,7 +101,7 @@ def decide_boundary(ch, label, i_name, data):
         return
     mean = data.mean()
     std_deviation = sqrt(data.var())
-    score = 95 if label == "Intro To Management" else 89
+    score = 92 if mean > 85 else 89
 
     boundaries = get_boundary(score, ch, mean, data)
     # Plot the new curve and save it
